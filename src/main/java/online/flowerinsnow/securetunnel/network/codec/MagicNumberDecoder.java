fea674bf37ac82d8d000730942173b706afb2c67
flowerinsnow <flowerimsnow@gmail.com>
2024-08-18 23:42:53 +0800

    docs(MagicNumberDecoder): 为MagicNumberDecoder补充Javadoc

--- src/main/java/online/flowerinsnow/securetunnel/network/codec/MagicNumberDecoder.java
+++ src/main/java/online/flowerinsnow/securetunnel/network/codec/MagicNumberDecoder.java
@@ -5,8 +5,16 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import online.flowerinsnow.securetunnel.exception.MagicNumberMismatchException;
 
+/**
+ * <p>魔数解码器</p>
+ * <p>会读取一次魔数并判断正确与否</p>
+ */
 public class MagicNumberDecoder extends ChannelInboundHandlerAdapter {
+    /**
+     * <p>已完成状态</p>
+     */
     private boolean done;
+
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         if (this.done) {
